f40f9b5aef8b3e29ae87f04d8ab70224093bf833
Toe Oo <tpso.toepyaesoneoo@gmail.com>
2020-10-10 15:16:25 +0630

    add bootstrap

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Home from '../../views/Home/Home'
 import About from '../../views/About/About'

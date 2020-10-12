fd6601bbd4b9822684f1c43b3e2539ddc8373497
Toe Oo <tpso.toepyaesoneoo@gmail.com>
2020-10-12 15:24:44 +0630

    setup store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
-import './index.css'
+import { Provider } from 'react-redux'
 import App from './App'
+import { store } from './store'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
